9c70035dff4910eb567b39409376fe2fecd81da1
Lucas Machado <lucas.dpmachado@gmail.com>
2024-05-11 11:25:12 -0300

    Test

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Wololo. Haha. Hehehe.
-        </p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+          Edit <code>src/App.js</code> and save to reload. Wololo. Haha. Hehehe. <!Company!><!Company!><!Company!>
+        </p><!Company!>
+        <img src={logo} className="App-logo" alt="logo" /><!Company!>
+        <p><!Team!>
           Edit <code>src/App.js</code> and save to reload. Wololo. Haha. Hehehe.
         </p>
         <img src={logo} className="App-logo" alt="logo" />
